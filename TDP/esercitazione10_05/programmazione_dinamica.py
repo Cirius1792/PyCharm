b0918af9364a9d8c3fb7d570082262d074af9401
CLT <Cirius1792@msn.com>
2017-05-11 17:00:05 +0200

    nn

--- TDP/esercitazione10_05/programmazione_dinamica.py
+++ TDP/esercitazione10_05/programmazione_dinamica.py
@@ -25,6 +25,61 @@ def edit_distance2(s1, s2):
                              v[j - 1] + 1,
                              v[j] + 1), v[j]
     return v[-1]
+def print_edit_transfer(d):
+    I = 'I'
+    R = 'R'
+    D = 'D'
+    M = 'M'
+    i, j = len(d)-1, len(d[0])-1
+    cur = d[i][j]
+    stk = []
+    while cur != 0:
+        if d[i-1][j] < cur:
+            stk.append(D)
+            i -= 1
+        elif d[i][j-1] < cur:
+            stk.append(I)
+            j -= 1
+        elif d[i-1][j-1] < cur:
+            stk.append(R)
+            j -= 1
+            i -= 1
+        else:
+            stk.append(M)
+            j -= 1
+            i -= 1
+        cur = d[i][j]
+    transcript = ""
+    for i in range(len(stk)):
+        transcript += stk.pop()
+    return transcript
+
+def coin_change(amount_rem, coin_combinations=[50, 20, 10, 5, 2, 1]):
+
+    #coin_combinations = [6, 2]
+    coin_list = []
+    for coin_val in coin_combinations:
+        coin_count = amount_rem//coin_val
+        coin = coin_val * coin_count
+        for i in range(int(coin_count)):
+            coin_list.append(coin_val)
+        amount_rem = round(amount_rem - coin_val * coin_count, 2)
+        if amount_rem == 0:
+            return coin_list
+    if amount_rem > 0:
+        return []
+
+def original_coin_change(amount_rem):
+    coin_combinations = [50, 20, 10, 5, 2, 1]  # coin_combina9ons = [6, 2]
+    coin_list = []
+    for coin_val in coin_combinations:
+        coin_count = amount_rem // coin_val
+        coin_list += [coin_val] * coin_count
+        amount_rem -= coin_val * coin_count
+    if amount_rem == 0:
+        return coin_list
+    if amount_rem > 0:
+        return []
 
 def print_matrix(mtr):
     for a in mtr:
@@ -41,3 +96,10 @@ if __name__ == "__main__":
 
     dist = edit_distance2(s1, s2)
     print(dist)
+
+    r = print_edit_transfer(mtr)
+    print(r)
+    print("\n\nCoin Change:")
+    money = 2.99
+    print("resto di",money, ':', coin_change(money, [2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01]))
+    #print("resto di", money, ':', coin_change(money))
