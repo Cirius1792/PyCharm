2ce356171d1a23bea768117306a0feeb6fe77dc7
CLT <Cirius1792@msn.com>
2017-05-16 12:54:49 +0200

    0/1 knapsak in programmazione_dinamica

--- TDP/esercitazione10_05/programmazione_dinamica.py
+++ TDP/esercitazione10_05/programmazione_dinamica.py
@@ -25,6 +25,7 @@ def edit_distance2(s1, s2):
                              v[j - 1] + 1,
                              v[j] + 1), v[j]
     return v[-1]
+
 def print_edit_transfer(d):
     I = 'I'
     R = 'R'
@@ -81,14 +82,31 @@ def original_coin_change(amount_rem):
     if amount_rem > 0:
         return []
 
+def knapsak(x, v, P):
+    n = len(x)
+    d = [[0] * P for x in range(0, n)]
+    a = [0 for i in range(0,n)]
+    for i in range(1,n):
+        for j in range(1, P):
+            if j >= x[i-1]:
+                if d[i - 1][j] > d[i - 1][j - x[i - 1]] + v[i - 1]:
+                    d[i][j] = d[i - 1][j]
+                    a[i - 1] = 0
+                else:
+                    d[i][j] = d[i - 1][j - x[i - 1]] + v[i - 1]
+                    a[i - 1] = 1
+            else:
+                d[i][j] = d[i-1][j]
+                a[i-1] = 0
+    return d, a
+
 def print_matrix(mtr):
     for a in mtr:
         ln = ""
         for i in a:
             ln += str(i) + " "
         print(ln)
-
-if __name__ == "__main__":
+def test1():
     s2 = "writers"
     s1 = "vintner"
     mtr = edit_distance(s1, s2)
@@ -103,3 +121,20 @@ if __name__ == "__main__":
     money = 2.99
     print("resto di",money, ':', coin_change(money, [2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01]))
     #print("resto di", money, ':', coin_change(money))
+
+def test_knapsak():
+
+    x = [3, 4, 5, 8, 10]
+    p = [2, 3, 4, 5, 9]
+    W = 20
+    d, a = knapsak(x, p, W)
+    print("matrice:")
+    print_matrix(d)
+    print("peso:\t\t\t",x)
+    print("valore:\t\t\t", p)
+    print("presi:\t\t\t", a)
+
+
+
+if __name__ == "__main__":
+    test_knapsak()
