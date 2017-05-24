1459ff350feb73a13cb0e489da26eb9661bcf558
CLT <Cirius1792@msn.com>
2017-05-24 15:33:56 +0200

    homework5 Finito

--- TDP/homework5/homework5_Ciro_Lucio_Tecce.py
+++ TDP/homework5/homework5_Ciro_Lucio_Tecce.py
@@ -86,7 +86,6 @@ def cycle_free(G):
             wgt = float(e.element())
             if d[u] + wgt < d[v]:
                 return True
-            
     return cycle
 
 def bf_cycle_check(G, s):
